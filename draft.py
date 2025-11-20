0576d178ff62a79c7ea5b667052254e250b974dd
nikunjsrivastav <nikunjshrivastav47@gmail.com>
2025-11-20 22:49:51 +0530

    Final Commit

--- draft.py
+++ draft.py
@@ -9,7 +9,7 @@ from openpyxl.styles import Alignment, Font, PatternFill, Border, Side
 random.seed(42)
 
 days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-excluded = ["07:30-09:00", "10:30-10:45", "13:15-14:00","17:30-18:30"]
+excluded = ["07:30-09:00", "10:30-10:45", "13:15-14:00"]
 
 colors = [
     "FFB3BA","BAE1FF","BAFFC9","FFFFBA","FFD8BA","E3BAFF","D0BAFF","FFCBA4",
@@ -101,6 +101,8 @@ def valid(c):
     return err
 def is_combined_course(code, rm):
     return (code, "L") in rm and rm[(code, "L")] == "C004"
+
+
 lab_prefix_for_class_prefix = {
     "C1": "L1",
     "C2": "L2",
@@ -156,7 +158,7 @@ def free(tt, d, ex=False):
     return fb
 
 def alloc_specific(tt, busy, rm, room_busy, day, slots_to_use, f, code, typ, elec, labsd, course_usage,
-                   class_prefix=None, rr_state=None,hide_c004=False):
+                   class_prefix=None, rr_state=None):
     for s_ in slots_to_use:
         if s_ not in slot_keys or tt.at[day, s_] != "":
             return False
@@ -166,17 +168,12 @@ def alloc_specific(tt, busy, rm, room_busy, day, slots_to_use, f, code, typ, ele
 
     usage = course_usage[day][code]
 
-# For electives: Do NOT treat P as a real lab hour
-    if typ == "P" and elec:
-    # Elective lab behaves like theory: allow unlimited placement
-        pass
+    if typ == "P":
+        if usage["P"] >= 1: 
+            return False
     else:
-        if typ == "P":
-            if usage["P"] >= 1:
-                return False
-        else:
-            if (usage["L"] + usage["T"]) >= 1:
-                return False
+        if (usage["L"] + usage["T"]) >= 1: 
+            return False
     r = None
     if not elec:
         key = (code, typ)
@@ -188,9 +185,7 @@ def alloc_specific(tt, busy, rm, room_busy, day, slots_to_use, f, code, typ, ele
                     return False
             r = candidate
         else:
-            if typ == "P" and elec:
-                r = None
-            elif typ == "P":
+            if typ == "P":
                 lab_pref = lab_prefix_for_class_prefix.get(class_prefix, None)
                 candidates = room_candidates(lab=True, prefix=None, lab_prefix=lab_pref)
             else:
@@ -201,38 +196,24 @@ def alloc_specific(tt, busy, rm, room_busy, day, slots_to_use, f, code, typ, ele
             rm[key] = r
 
     for s_ in slots_to_use:
-        if is_combined_course(code, rm):
-            if hide_c004:
-                if typ == "P":
-                    v = f"{code} (Lab)"
-                elif typ == "T":
-                    v = f"{code}T"
-                else:
-                    v = f"{code}"
-            else:
+        if r and not elec:
+            if is_combined_course(code, rm):
                 if typ == "P":
                     v = f"{code} (Lab)"
                 elif typ == "T":
                     v = f"{code}T (C004)"
                 else:
                     v = f"{code} (C004)"
-        else:
-            if r and not elec:
-                if elec and typ == "P":
-                    v = f"{code}(Lab)"
-                elif typ == "T": 
-                    v = f"{code}T ({r})"
-                elif typ == "P": 
-                    v = f"{code} (Lab-{r})"
-                else: 
-                    v = f"{code} ({r})"
             else:
-                if elec and typ == "P":
-                    v = f"{code}(Lab)"
-                elif typ == "T":
-                    v = f"{code}T"
-                else:
-                    v = code
+                if typ == "T": 
+                        v = f"{code}T ({r})"
+                elif typ == "P": 
+                        v = f"{code} (Lab-{r})"
+                else: 
+                        v = f"{code} ({r})"
+
+        else:
+            v = f"{code}T" if typ == "T" else code
         tt.at[day, s_] = v
 
     if f:
@@ -245,7 +226,7 @@ def alloc_specific(tt, busy, rm, room_busy, day, slots_to_use, f, code, typ, ele
     return True
 
 def alloc(tt, busy, rm, room_busy, d, f, code, h, typ="L", elec=False, labsd=set(), ex=False,
-          preferred_slots=None, course_usage=None, class_prefix=None, rr_state=None,hide_c004=False):
+          preferred_slots=None, course_usage=None, class_prefix=None, rr_state=None):
     if course_usage is None:
         course_usage = {dd:{} for dd in days}
     if code not in course_usage[d]:
@@ -266,7 +247,7 @@ def alloc(tt, busy, rm, room_busy, d, f, code, h, typ="L", elec=False, labsd=set
         if pref_day == d:
             total = sum(slot_dur[s] for s in pref_slots)
             if total + 1e-9 >= h:
-                if alloc_specific(tt, busy, rm, room_busy, pref_day, pref_slots, f, code, typ, elec, labsd, course_usage, class_prefix=class_prefix, rr_state=rr_state,hide_c004=hide_c004):
+                if alloc_specific(tt, busy, rm, room_busy, pref_day, pref_slots, f, code, typ, elec, labsd, course_usage, class_prefix=class_prefix, rr_state=rr_state):
                     return True
 
     for blk in free(tt, d, ex):
@@ -287,9 +268,7 @@ def alloc(tt, busy, rm, room_busy, d, f, code, h, typ="L", elec=False, labsd=set
                     if set(use) & used:
                         continue
             else:
-                if typ == "P" and elec:
-                    r = None
-                elif typ == "P":
+                if typ == "P":
                     lab_pref = lab_prefix_for_class_prefix.get(class_prefix, None)
                     candidates = room_candidates(lab=True, prefix=None, lab_prefix=lab_pref)
                     r = pick_room_for_slots(candidates, d, use, room_busy, rr_state_key=lab_pref, rr_state=rr_state)
@@ -304,39 +283,26 @@ def alloc(tt, busy, rm, room_busy, d, f, code, h, typ="L", elec=False, labsd=set
             r = None
 
         for s_ in use:
-            if is_combined_course(code, rm):
-                if hide_c004:
-                    if typ == "P":
-                        v = f"{code}(Lab)"
-                    elif typ == "T":
-                        v = f"{code}T"
-                    else:
-                        v = f"{code}"
-                else:
+            if r and not elec:
+                if is_combined_course(code, rm):
                     if typ == "P":
                         v = f"{code} (Lab)"
                     elif typ == "T":
                         v = f"{code}T (C004)"
                     else:
                         v = f"{code} (C004)"
-            else:
-                if r and not elec:
-                    if elec and typ == "P":
-                        v = f"{code}(Lab)"
-                    elif typ == "T": 
+                else:
+                    if typ == "T": 
                         v = f"{code}T ({r})"
                     elif typ == "P": 
                         v = f"{code} (Lab-{r})"
                     else: 
                         v = f"{code} ({r})"
-                else:
-                    if elec and typ == "P":
-                        v = f"{code}(Lab)"
-                    elif typ == "T":
-                        v = f"{code}T"
-                    else:
-                        v = code
+
+            else:
+                v = f"{code}T" if typ == "T" else code
             tt.at[d, s_] = v
+
         if f:
             busy[d].setdefault(f, set()).update(use)
         if r:
@@ -378,24 +344,27 @@ def extract_contiguous_blocks(slot_list):
     return blocks
 
 def try_allocate_chunk_from_block(tt, busy, rm, room_busy, labsd, course_usage,
-                                  code, faculty, typ, need, day, slots, class_prefix=None, rr_state=None,hide_c004=False):
+                                  code, faculty, typ, need, day, slots, class_prefix=None, rr_state=None):
     n = len(slots)
     for i in range(n):
         accum = 0.0; sub = []
         for j in range(i, n):
             sub.append(slots[j]); accum += slot_dur[slots[j]]
             if accum + 1e-9 >= need:
+                used = room_busy.get(day, {}).get("C004", set())
+                if set(sub) & used:
+                    break
                 for s_ in sub:
                     if tt.at[day, s_] != "": break
                 else:
-                    ok = alloc_specific(tt, busy, rm, room_busy, day, sub, faculty, code, typ, False, labsd, course_usage, class_prefix=class_prefix, rr_state=rr_state,hide_c004=hide_c004)
+                    ok = alloc_specific(tt, busy, rm, room_busy, day, sub, faculty, code, typ, False, labsd, course_usage, class_prefix=class_prefix, rr_state=rr_state)
                     if ok:
                         new_slots = slots[:i] + slots[j+1:]
                         return new_slots
                 break
     return None
 
-def assign_combined_precise_durations(tt, busy, rm, room_busy, labsd, course_usage, combined_core, rr_state=None,hide_c004=False):
+def assign_combined_precise_durations(tt, busy, rm, room_busy, labsd, course_usage, combined_core, rr_state=None):
     if not combined_core:
         return []
     combined_list = []
@@ -443,7 +412,7 @@ def assign_combined_precise_durations(tt, busy, rm, room_busy, labsd, course_usa
                 if day in days_used: continue
                 new_slots = try_allocate_chunk_from_block(tt, busy, rm, room_busy, labsd, course_usage,
                                                           code, faculty, typ, need, day, slots,
-                                                          class_prefix="C0", rr_state=rr_state,hide_c004=hide_c004)
+                                                          class_prefix="C0", rr_state=rr_state)
                 if new_slots is not None:
                     valid_blocks[idx] = (day, new_slots); days_used.add(day); allocated = True; break
             if not allocated:
@@ -451,10 +420,10 @@ def assign_combined_precise_durations(tt, busy, rm, room_busy, labsd, course_usa
                     if day in days_used: continue
                     new_slots = try_allocate_chunk_from_block(tt, busy, rm, room_busy, labsd, course_usage,
                                                               code, faculty, typ, need, day, slots,
-                                                              class_prefix="C0", rr_state=rr_state,hide_c004=hide_c004)
+                                                              class_prefix="C0", rr_state=rr_state)
                     if new_slots is not None:
                         excluded_blocks[idx] = (day, new_slots); days_used.add(day); allocated = True; break
-        placed.append(code)
+        placed.append(c)
     return placed
 
 color_avail = colors.copy(); random.shuffle(color_avail); color_map = {}
@@ -599,8 +568,7 @@ def add_csv_legend_block(ws, csv_path, legend_title, room_prefix=None, elective_
 
     df["Elective Room"] = elective_rooms
 
-    headers = ["Course Code","Course Title","L-T-P-S-C","Faculty","Semester Half","Elective","Elective Basket","Elective Room"]
-
+    headers = ["Course Code","Course Title","L-T-P-S-C","Faculty","Semester Half","Elective","Elective Room"]
     ws.append(headers); header_row = ws.max_row
     for i, _h in enumerate(headers, start=1):
         c = ws.cell(header_row, i); c.font = Font(bold=True); c.alignment = Alignment(horizontal="center", vertical="center"); c.border = thin; c.fill = PatternFill(start_color="D9D9D9", end_color="D9D9D9", fill_type="solid")
@@ -613,7 +581,6 @@ def add_csv_legend_block(ws, csv_path, legend_title, room_prefix=None, elective_
             s(row["Faculty"]),
             s(row["Semester_Half"]),
             s(row["Elective"]),
-            s(row["ElectiveBasket"]),
             row["Elective Room"]
         ]
         ws.append(rowvals)
@@ -621,7 +588,7 @@ def add_csv_legend_block(ws, csv_path, legend_title, room_prefix=None, elective_
             cc = ws.cell(ws.max_row, i); cc.alignment = Alignment(horizontal="center", vertical="center", wrap_text=True); cc.border = thin
     ws.append([""])
 
-def generate(courses, ws, label, seed, elective_sync, room_prefix=None, elective_room_map=None, room_busy_global=None,hide_c004=False):
+def generate(courses, ws, label, seed, elective_sync, room_prefix=None, elective_room_map=None, room_busy_global=None):
     if elective_room_map is None:
         elective_room_map = {}
     if valid(courses): return []
@@ -701,26 +668,38 @@ def generate(courses, ws, label, seed, elective_sync, room_prefix=None, elective
 
                     if sync_name and sync_name in elective_sync:
                         pref = elective_sync[sync_name]
-                        if alloc(tt, busy, rm, room_busy, pref["day"], f, code, a, typ, is_elec_flag, labsd, False, preferred_slots=(pref["day"], pref["slots"]), course_usage=course_usage, class_prefix=room_prefix, rr_state=rr_state,hide_c004=hide_c004):
+                        if alloc(tt, busy, rm, room_busy, pref["day"], f, code, a, typ, is_elec_flag, labsd, False, preferred_slots=(pref["day"], pref["slots"]), course_usage=course_usage, class_prefix=room_prefix, rr_state=rr_state):
                             h -= a; placed = True
 
                     if not placed:
-                        for i in range(5):
-                            if is_elec_flag:
-                                d_order = days[:]
-                            else:
-                                start_idx = start_idx_ref[0]
-                                d_order = days[start_idx:] + days[:start_idx]
-                                start_idx_ref[0] = (start_idx_ref[0] + 1) % len(days)
-                            for d in d_order:
-                                if alloc(tt, busy, rm, room_busy, d, f, code, a, typ, is_elec_flag, labsd, False, course_usage=course_usage, class_prefix=room_prefix, rr_state=rr_state,hide_c004=hide_c004):
-                                    h -= a; placed = True; break
-                            if placed:
-                                break
-                    if not placed:
-                        for d in days:
-                            if alloc(tt, busy, rm, room_busy, d, f, code, a, typ, is_elec_flag, labsd, True, course_usage=course_usage, class_prefix=room_prefix, rr_state=rr_state,hide_c004=hide_c004):
+                        if is_elec_flag:
+                            d_order = days[:]
+                        else:
+                            start_idx = start_idx_ref[0]
+                            d_order = days[start_idx:] + days[:start_idx]
+                            start_idx_ref[0] = (start_idx_ref[0] + 1) % len(days)
+                        for d in d_order:
+                            if alloc(tt, busy, rm, room_busy, d, f, code, a, typ, is_elec_flag, labsd, False, course_usage=course_usage, class_prefix=room_prefix, rr_state=rr_state):
                                 h -= a; placed = True; break
+                    if not placed:
+
+                        exists_normal_space = False
+                        for dcheck in days:
+                            free_blocks = free(tt, dcheck, ex=False)
+                            for b in free_blocks:
+                                if sum(slot_dur[s] for s in b) + 1e-9 >= a:
+                                    exists_normal_space = True
+                                    break
+                            if exists_normal_space:
+                                break
+                        if not exists_normal_space:
+                            for d in days:
+                                if alloc(tt, busy, rm, room_busy, d, f, code, a, typ, is_elec_flag,
+                                        labsd, True, course_usage=course_usage,
+                                        class_prefix=room_prefix, rr_state=rr_state):
+                                    h -= a
+                                    placed = True
+                                    break
 
                     if placed and sync_name and sync_name not in elective_sync:
                         for dcheck in days:
@@ -740,18 +719,17 @@ def generate(courses, ws, label, seed, elective_sync, room_prefix=None, elective
 
     start_idx_ref = [seed % len(days)]
     elec_final.sort(key=lambda x: 0 if x.get("_sync_name") in elective_sync else 1)
-    
+    combined_placed = assign_combined_precise_durations(tt, busy, rm, room_busy,
+                                                    labsd, course_usage,
+                                                    combined_core,
+                                                    rr_state=rr_state)
     priority_placed = place_course_list(elec_final, start_idx_ref)
-
-    combined_placed = assign_combined_precise_durations(tt, busy, rm, room_busy, labsd, course_usage, combined_core, rr_state=rr_state, hide_c004=hide_c004)
-
     regular_placed = place_course_list(regular_core, start_idx_ref)
-
     ws.append(["Day"] + slot_keys)
     for d in days:
         ws.append([d] + [tt.at[d, s] for s in slot_keys])
     ws.append([""])
-    return (priority_placed + regular_placed + combined_core)
+    return (combined_placed + priority_placed + regular_placed)
 def split(c):
     f = [x for x in c if s(x.get("Semester_Half","")) in ["1","0"]]
     s2 = [x for x in c if s(x.get("Semester_Half","")) in ["2","0"]]
@@ -774,12 +752,12 @@ if __name__ == "__main__":
     cAf, cAs = split(coursesAI)
     cBf, cBs = split(coursesBI)
     
-    csea_block = generate(cAf, ws1, "CSEA I First Half", seed+0, sync_sem1, room_prefix='C1', elective_room_map=elective_room_map, room_busy_global=global_room_busy,hide_c004=True)
-    csea_block2 = generate(cAs, ws1, "CSEA I Second Half", seed+1, sync_sem1, room_prefix='C1', elective_room_map=elective_room_map, room_busy_global=global_room_busy,hide_c004=True)
+    csea_block = generate(cAf, ws1, "CSEA I First Half", seed+0, sync_sem1, room_prefix='C1', elective_room_map=elective_room_map, room_busy_global=global_room_busy)
+    csea_block2 = generate(cAs, ws1, "CSEA I Second Half", seed+1, sync_sem1, room_prefix='C1', elective_room_map=elective_room_map, room_busy_global=global_room_busy)
     add_csv_legend_block(ws1, "data/coursesCSEA-I.csv", "CSEA I", room_prefix="C1", elective_room_map=elective_room_map)
     
-    cseb_block = generate(cBf, ws1, "CSEB I First Half", seed+2, sync_sem1, room_prefix='C1', elective_room_map=elective_room_map, room_busy_global=global_room_busy,hide_c004=True)
-    cseb_block2 = generate(cBs, ws1, "CSEB I Second Half", seed+3, sync_sem1, room_prefix='C1', elective_room_map=elective_room_map, room_busy_global=global_room_busy,hide_c004=True)
+    cseb_block = generate(cBf, ws1, "CSEB I First Half", seed+2, sync_sem1, room_prefix='C1', elective_room_map=elective_room_map, room_busy_global=global_room_busy)
+    cseb_block2 = generate(cBs, ws1, "CSEB I Second Half", seed+3, sync_sem1, room_prefix='C1', elective_room_map=elective_room_map, room_busy_global=global_room_busy)
     add_csv_legend_block(ws1, "data/coursesCSEB-I.csv", "CSEB I", room_prefix="C1", elective_room_map=elective_room_map)
     
     combined_i_courses = (csea_block or []) + (csea_block2 or []) + (cseb_block or []) + (cseb_block2 or [])
